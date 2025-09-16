6ee328593b76b090ef152f3443d0d02d49297d43
jitendra-121 <166691303+jitendra-121@users.noreply.github.com>
2025-09-16 14:55:16 +0530

    add BOW feature engineering

--- emotion-detection/src/models/model_evaluation.py
+++ emotion-detection/src/models/model_evaluation.py
@@ -87,7 +87,7 @@ def save_metrics(metrics: dict, file_path: str) -> None:
 def main():
     try:
         clf = load_model('emotion-detection/models/model.pkl')
-        test_data = load_data('emotion-detection/data/processed/test_tfidf.csv')
+        test_data = load_data('emotion-detection/data/processed/test_bow.csv')
         
         X_test = test_data.iloc[:, :-1].values
         y_test = test_data.iloc[:, -1].values
